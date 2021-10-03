ba364af6f29a5834e201e4b606423c9bb0ca7162
Ociba James <ocibajames@gmail.com>
2021-10-03 14:03:11 +0300

    footer

--- resources/views/frontpages/G/index.blade.php
+++ resources/views/frontpages/G/index.blade.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-    
-<!-- Mirrored from instantcashers.com/members/G/ by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 27 Sep 2021 12:39:30 GMT -->
-<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 <head>
         
 
@@ -470,89 +468,53 @@ that allows you to build websites for any purpose. Yes, from blog to corporate m
                 </div>
             </div>
                 
-        <footer class="footer">
+            <footer class="footer">
             <div class="container">
-                
-
-                                    <ul class="nav-footer nav-footer-social">
-                        <li><a href="#" class="twitter"><span class="fa fa-twitter"></span></a></li>                        <li><a href="#" class="facebook"><span class="fa fa-facebook"></span></a></li>                        <li><a href="#" class="google-plus"><span class="fa fa-google-plus"></span></a></li>                        <li><a href="#" class="linkedin"><span class="fa fa-linkedin"></span></a></li>                    </ul>
-                
-                
-        
-
-            <ul class="nav-footer ">
-
-            
-                
-                                    <li class="m_about
-                                                                                                                        ">
-
-                        <a href="../../about/index.html"
-                                                                                                            >
-                            About Us
-                                                    </a>
-                                            </li>
-                
-                
-            
-                
-                                    <li class="m_policy
-                                                                                                                        ">
-
-                        <a href="../../policy/index.html"
-                                                                                                            >
-                            Privacy Policy
-                                                    </a>
-                                            </li>
-                
-                
-            
-                
-                                    <li class="m_terms
-                                                                                                                        ">
-
-                        <a href="../../terms/index.html"
-                                                                                                            >
-                            Terms of Use
-                                                    </a>
-                                            </li>
-                
-                
-            
-                
-                                    <li class="m_help
-                                                                                                                        ">
-
-                        <a href="../../help/index.html"
-                                                                                                            >
-                            Help
-                                                    </a>
-                                            </li>
-                
-                
-            
-                
-                                    <li class="m_blog
-                                                                                                                        ">
-
-                        <a href="../../blog/index.html"
-                                                                                                            >
-                            Blog
-                                                    </a>
-                                            </li>
-                
-                
-            
-            <!-- MORE menu dropdown -->
-                    </ul>
-    
-
-
-
-
-                <p class="copyright">&copy; 2021 Powered by <a href="https://subrion.org/" title="Open Source CMS">Subrion CMS</a></p>
-
-                
+                <ul class="nav-footer nav-footer-social">
+                    <li><a href="#" class="twitter"><span class="fa fa-twitter"></span></a></li>
+                    <li><a href="#" class="facebook"><span class="fa fa-facebook"></span></a></li>
+                    <li><a href="#" class="google-plus"><span class="fa fa-google-plus"></span></a></li>
+                    <li><a href="#" class="linkedin"><span class="fa fa-linkedin"></span></a></li>
+                </ul>
+                <ul class="nav-footer ">
+                    <li class="m_about
+                        ">
+                        <a href="/about"
+                            >
+                        About Us
+                        </a>
+                    </li>
+                    <li class="m_policy
+                        ">
+                        <a href="/policy"
+                            >
+                        Privacy Policy
+                        </a>
+                    </li>
+                    <li class="m_terms
+                        ">
+                        <a href="/terms"
+                            >
+                        Terms of Use
+                        </a>
+                    </li>
+                    <li class="m_help
+                        ">
+                        <a href="/help"
+                            >
+                        Help
+                        </a>
+                    </li>
+                    <li class="m_blog
+                        ">
+                        <a href="/blog"
+                            >
+                        Blog
+                        </a>
+                    </li>
+                    <!-- MORE menu dropdown -->
+                </ul>
+                <p class="copyright">&copy; {{date('Y')}} Powered by <a href="https://subrion.org/" title="Open Source CMS">Subrion CMS</a></p>
             </div>
         </footer>
 
--- resources/views/frontpages/Y/index.blade.php
+++ resources/views/frontpages/Y/index.blade.php
@@ -468,89 +468,53 @@ that allows you to build websites for any purpose. Yes, from blog to corporate m
                 </div>
             </div>
                 
-        <footer class="footer">
+            <footer class="footer">
             <div class="container">
-                
-
-                                    <ul class="nav-footer nav-footer-social">
-                        <li><a href="#" class="twitter"><span class="fa fa-twitter"></span></a></li>                        <li><a href="#" class="facebook"><span class="fa fa-facebook"></span></a></li>                        <li><a href="#" class="google-plus"><span class="fa fa-google-plus"></span></a></li>                        <li><a href="#" class="linkedin"><span class="fa fa-linkedin"></span></a></li>                    </ul>
-                
-                
-        
-
-            <ul class="nav-footer ">
-
-            
-                
-                                    <li class="m_about
-                                                                                                                        ">
-
-                        <a href="../../about/index.html"
-                                                                                                            >
-                            About Us
-                                                    </a>
-                                            </li>
-                
-                
-            
-                
-                                    <li class="m_policy
-                                                                                                                        ">
-
-                        <a href="../../policy/index.html"
-                                                                                                            >
-                            Privacy Policy
-                                                    </a>
-                                            </li>
-                
-                
-            
-                
-                                    <li class="m_terms
-                                                                                                                        ">
-
-                        <a href="../../terms/index.html"
-                                                                                                            >
-                            Terms of Use
-                                                    </a>
-                                            </li>
-                
-                
-            
-                
-                                    <li class="m_help
-                                                                                                                        ">
-
-                        <a href="../../help/index.html"
-                                                                                                            >
-                            Help
-                                                    </a>
-                                            </li>
-                
-                
-            
-                
-                                    <li class="m_blog
-                                                                                                                        ">
-
+                <ul class="nav-footer nav-footer-social">
+                    <li><a href="#" class="twitter"><span class="fa fa-twitter"></span></a></li>
+                    <li><a href="#" class="facebook"><span class="fa fa-facebook"></span></a></li>
+                    <li><a href="#" class="google-plus"><span class="fa fa-google-plus"></span></a></li>
+                    <li><a href="#" class="linkedin"><span class="fa fa-linkedin"></span></a></li>
+                </ul>
+                <ul class="nav-footer ">
+                    <li class="m_about
+                        ">
+                        <a href="/about"
+                            >
+                        About Us
+                        </a>
+                    </li>
+                    <li class="m_policy
+                        ">
+                        <a href="/policy"
+                            >
+                        Privacy Policy
+                        </a>
+                    </li>
+                    <li class="m_terms
+                        ">
+                        <a href="/terms"
+                            >
+                        Terms of Use
+                        </a>
+                    </li>
+                    <li class="m_help
+                        ">
+                        <a href="/help"
+                            >
+                        Help
+                        </a>
+                    </li>
+                    <li class="m_blog
+                        ">
                         <a href="/blog"
-                                                                                                            >
-                            Blog
-                                                    </a>
-                                            </li>
-                
-                
-            
-            <!-- MORE menu dropdown -->
-                    </ul>
-    
-
-
-
-
-                <p class="copyright">&copy; 2021 Powered by <a href="https://subrion.org/" title="Open Source CMS">Subrion CMS</a></p>
-
-                
+                            >
+                        Blog
+                        </a>
+                    </li>
+                    <!-- MORE menu dropdown -->
+                </ul>
+                <p class="copyright">&copy; {{date('Y')}} Powered by <a href="https://subrion.org/" title="Open Source CMS">Subrion CMS</a></p>
             </div>
         </footer>
 
